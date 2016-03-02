8e7428a1af0480917180f89ec52f66bce710baf7
Dalius Šidlauskas <dalius.sidlauskas@gmail.com>
2016-03-02 12:33:17 +0200

    Configure maven to run E2E tests

--- src/it/scala/env/GlobalTestEnvSupport.scala
+++ src/it/scala/env/GlobalTestEnvSupport.scala
@@ -4,24 +4,15 @@ import java.io.{PrintWriter, StringWriter}
 
 import org.specs2.execute.{AsResult, Error, Result}
 import org.specs2.specification.AroundEach
-import org.specs2.specification.core.{Description, Fragments, SpecificationStructure}
-import org.specs2.specification.create.FragmentsFactory
+import org.specs2.specification.core.SpecificationStructure
 
-import scala.util.{Try, Success, Failure}
+import scala.util.{Failure, Success, Try}
 
-trait GlobalTestEnvSupport extends AroundEach with BeforeAllWithNamedStep {
+trait GlobalTestEnvSupport extends AroundEach {
   self: SpecificationStructure =>
 
   def testEnv: Try[ManagedService]
 
-  final override val beforeAllStepName = "Step - Env Setup"
-
-  final override def beforeAll: Unit = {
-    testEnv match {
-      case Failure(e) => printEnvironmentInitError(e)
-      case _ =>
-    }
-  }
 
   final override def around[T: AsResult](t: => T): Result = {
     testEnv match {
@@ -38,16 +29,5 @@ trait GlobalTestEnvSupport extends AroundEach with BeforeAllWithNamedStep {
     val stringWriter = new StringWriter
     t.printStackTrace(new PrintWriter(stringWriter))
     println(stringWriter.toString)
-  }
-}
-
-private[env] trait BeforeAllWithNamedStep extends SpecificationStructure with FragmentsFactory {
-  def beforeAll: Unit
-
-  def beforeAllStepName: String
-
-  override def map(fs: => Fragments) = {
-    val beforeAllFragment = fragmentFactory.step(beforeAll).copy(description = Description.text(beforeAllStepName))
-    super.map(fs).prepend(beforeAllFragment)
   }
 }

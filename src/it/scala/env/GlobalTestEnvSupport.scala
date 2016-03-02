3aa7a4704b3495263b42fc89a24ce7f24bdc3dc9
Dalius Šidlauskas <dalius.sidlauskas@gmail.com>
2016-03-02 12:36:10 +0200

    Clean code

--- src/it/scala/env/GlobalTestEnvSupport.scala
+++ src/it/scala/env/GlobalTestEnvSupport.scala
@@ -1,7 +1,5 @@
 package env
 
-import java.io.{PrintWriter, StringWriter}
-
 import org.specs2.execute.{AsResult, Error, Result}
 import org.specs2.specification.AroundEach
 import org.specs2.specification.core.SpecificationStructure
@@ -13,21 +11,11 @@ trait GlobalTestEnvSupport extends AroundEach {
 
   def testEnv: Try[ManagedService]
 
-
   final override def around[T: AsResult](t: => T): Result = {
     testEnv match {
       case Success(_) => AsResult(t)
       case Failure(_) => Error("Failed to initialize test environment")
       case _ => Error(s"Invalid state $testEnv during test execution")
     }
-  }
-
-  private def printEnvironmentInitError(t: Throwable): Unit = {
-    println("-------------------------------------------------------------------")
-    println(s"Environment failed. Reason: ${t.getMessage}")
-
-    val stringWriter = new StringWriter
-    t.printStackTrace(new PrintWriter(stringWriter))
-    println(stringWriter.toString)
   }
 }

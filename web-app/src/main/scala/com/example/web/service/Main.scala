47dcd7d9b92efefc28401d0b64ab4bf2a89bf106
Dalius Šidlauskas <dalius.sidlauskas@gmail.com>
2016-03-07 20:59:19 +0200

    Fix indentation
    Fix indentation

--- web-app/src/main/scala/com/example/web/service/Main.scala
+++ web-app/src/main/scala/com/example/web/service/Main.scala
@@ -4,8 +4,7 @@ import com.example.web.service.spring.SpringConfig
 import org.springframework.boot.SpringApplication
 
 object Main extends App {
-
-	override def main(args: Array[String]) {
-		SpringApplication.run(classOf[SpringConfig], args:_*)
-	}
+  override def main(args: Array[String]) {
+    SpringApplication.run(classOf[SpringConfig], args:_*)
+  }
 }
